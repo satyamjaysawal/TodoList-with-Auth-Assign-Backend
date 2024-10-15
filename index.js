14a8f696e1ef00422c10d8e7662387ab6441a769
Satyam_jaysawal <sjrecm9258@gmail.com>
2024-10-15 21:49:41 +0530

    first

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use('/api/todos', todoRoutes);
 
+// Default route
+app.get('/', (req, res) => {
+    res.send('Server is running');
+  });
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
